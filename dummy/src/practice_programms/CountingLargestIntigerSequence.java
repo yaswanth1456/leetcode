dc3329f11988234f70648d841fb1604c83bae270
yaswanth1456 <prakashkomitla@gmail.com>
2019-09-20 12:03:44 +0530

    largest intiger added

---
+++ dummy/src/practice_programms/CountingLargestIntigerSequence.java
@@ -0,0 +1,34 @@
+package practice_programms;
+
+public class CountingLargestIntigerSequence {
+	int counter;
+	int sum;
+	int a,b,j;
+	int max;
+	int[] array = new int[] { 2, 4, -13, 6, 7, 88, 3 };
+
+	public void sequenceCounting() {
+		for (int i = 0; i < array.length - 1; i++) {
+			counter++;
+			
+
+				for (j = i; j <= counter; j++) {
+					sum = sum + array[j];
+					System.out.println(array[j]);
+						}
+				if (sum>max) {
+                  max=sum;
+                	a=i;
+                	b=j;
+                	
+				}
+				System.out.println("sum"+ sum+" "+max);
+			}
+		
+
+	}
+	public static void main(String[] args) {
+		CountingLargestIntigerSequence cl = new CountingLargestIntigerSequence();
+		cl.sequenceCounting();
+	}
+}
